dc5f637fbffb8b119846fa062cb24c26434b50bd
ISRAEL MAGALHÃES DOS SANTOS JUNIOR <24122293@CFP127ALUNO.LOCAL>
2025-02-06 10:41:21 -0300

    Terminando o menu

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-"use strict"
+'use strict'
+
+
+// Criando um objeto para armazenas os conteudos
+
+const disciplinas = [
+    {nome: 'PPDM', icon: 'android.png', cor:'purple'},
+    {nome: 'BACK', icon: 'api.png', cor:'red'},
+    {nome: 'FRONT', icon: 'http.png', cor:'pink'},
+    {nome: 'PROJ', icon: 'projetos.png', cor:'plum'}
+]
+function criarMenu(disciplina){
+
+    
+
+    // Criando Tags
+    const novoItem = document.createElement('li')
+    const novaImagem = document.createElement('img')
+    const novoSpan = document.createElement('span')
+    const lista = document.getElementById('menu')
+
+    // Adicionando conteudo nas Tags filhas
+    novaImagem.src = `./img/${disciplina.icon}`
+    novoSpan.textContent = disciplina.nome
+
+    // Adicionando conteudo na tag mae
+    novoItem.appendChild(novaImagem)
+    novoItem.appendChild(novoSpan)
+    novoItem.style = `--cor-hover: ${disciplina.cor}`
+
+    lista.appendChild(novoItem)
+    
+}
+
+disciplinas.forEach(criarMenu)
