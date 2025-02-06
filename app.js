ed0aad92bd5d9c1a7c80a0b88fd3636cb78c9108
ISRAEL MAGALHÃES DOS SANTOS JUNIOR <24122293@CFP127ALUNO.LOCAL>
2025-02-06 12:22:38 -0300

    Finalizando o card e começando o JS do mesmo

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ const disciplinas = [
 ]
 function criarMenu(disciplina){
 
-    
-
     // Criando Tags
     const novoItem = document.createElement('li')
     const novaImagem = document.createElement('img')
@@ -31,5 +29,50 @@ function criarMenu(disciplina){
     lista.appendChild(novoItem)
     
 }
+
+const produtos = [
+    {
+        nome: 'Kotlin',
+        descricao: 'Aprenda os fundamentos da linguagem Kotlin, desde a instalação do ambiente até a criação de projetos simples. Ideal para quem deseja iniciar sua jornada no desenvolvimento Android ou em outras plataformas',
+        photo: 'kotlin.png',
+        preco: 'R$ 390,90',
+        duration:'Duração',
+        duracoes: ['D1','D2','D3','D4','D5'],
+        cart: 'ADD TO CART'
+    },
+    {
+        nome: 'FRONT-END',
+        descricao: 'Torne-se um desenvolvedor front-end completo! Aprenda a construir interfaces modernas e responsivas com HTML, CSS e JavaScript, além de entender como funciona o desenvolvimento web',
+        photo: 'front_end.png',
+        preco: 'R$ 499,90',
+        duration:'Duração',
+        duracoes: ['D1','D2','D3','D4','D5'],
+        cart: 'ADD TO CART'
+    },
+    {
+        nome: 'BACK-END',
+        descricao: 'Construa sistemas robustos e escaláveis com Java e Spring Boot. Este curso ensina desde o básico até a criação de APIs RESTful, passando por autenticação e banco de dados.',
+        photo: 'backend.png',
+        preco: 'R$ 599,90',
+        duration:'Duração',
+        duracoes: ['D1','D2','D3','D4','D5'],
+        cart: 'ADD TO CART'
+    },
+    {
+        nome: 'Projetos',
+        descricao: 'Aprenda a gerenciar projetos de software utilizando metodologias ágeis como Scrum e Kanban, com foco na integração entre equipes de desenvolvimento e stakeholders',
+        photo: 'projetos.png',
+        preco: 'R$ 449,90',
+        duration:'Duração',
+        duracoes: ['D1','D2','D3','D4','D5'],
+        cart: 'ADD TO CART'
+    }
+]
+
+function criarCards(produto){
+    const novoGrupao = document.createElement('div')
+    const novoCard = document.createElement('div')
+}
+
 
 disciplinas.forEach(criarMenu)
