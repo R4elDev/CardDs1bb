f2e04e0185b86a0f08eca487d49435d3b7b62d63
ISRAEL MAGALHÃES DOS SANTOS JUNIOR <24122293@CFP127ALUNO.LOCAL>
2025-02-13 11:54:34 -0300

    Terminando JS

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const disciplinas = [
     {nome: 'FRONT', icon: 'http.png', cor:'pink'},
     {nome: 'PROJ', icon: 'projetos.png', cor:'plum'}
 ]
+
+
 function criarMenu(disciplina){
 
     // Criando Tags
@@ -70,9 +72,97 @@ const produtos = [
 ]
 
 function criarCards(produto){
-    const novoGrupao = document.createElement('div')
-    const novoCard = document.createElement('div')
+
+    
+
+    const container = document.getElementById('container')
+    const card = document.createElement('div')
+    card.className = 'card'
+
+
+    // Div Textos
+    const textos = document.createElement('div')
+    textos.className = 'textos'
+
+    const titulo = document.createElement('div')
+    titulo.className = 'titulo'
+    const tituloH = document.createElement('h1')
+
+    const desc = document.createElement('div')
+    desc.className = 'desc'
+    const descP = document.createElement('p')
+
+    // Div Foto
+    const image = document.createElement('div')
+    image.className = 'foto'
+    const img = document.createElement('img')
+
+    // Div Variacoes 
+    const variacoes = document.createElement('div')
+    variacoes.className = 'variacoes'
+    const valor = document.createElement('div')
+    valor.className = 'valor'
+    const valorH = document.createElement('h1')
+    const subtitulo = document.createElement('div')
+    subtitulo.className = 'subtitulo'
+    const subtituloH = document.createElement('h1')
+    
+    const duracao = document.createElement('div')
+    duracao.className = 'duracao'
+
+    const duracoes = document.createElement('div')
+    duracoes.className = 'duracoes'
+    const duracoesP = document.createElement('p')
+
+
+    const carrinho = document.createElement('div')
+    carrinho.className = 'carrinho'
+
+    const carrinhoH = document.createElement('h1')
+
+
+    // Adicionando valor
+    tituloH.textContent = produto.nome
+    descP.textContent = produto.descricao
+    img.src = `./imgCard/${produto.photo}`
+    valorH.textContent = produto.preco
+    subtituloH.textContent = produto.duration
+
+    produtos.forEach(function(item){
+        duracoesP.textContent = item.duracoes.join(' ')
+    })
+
+
+
+    carrinhoH.textContent = produto.cart
+
+
+    // Adicionando aos pais
+    carrinho.appendChild(carrinhoH)
+    duracoes.appendChild(duracoesP)
+    duracao.appendChild(duracoes)
+    variacoes.appendChild(duracao)
+    valor.appendChild(valorH)
+    subtitulo.appendChild(subtituloH)
+    variacoes.appendChild(subtitulo)
+    variacoes.appendChild(valor)
+    
+
+    image.appendChild(img)
+    titulo.appendChild(tituloH)
+    desc.appendChild(descP)
+    textos.replaceChildren(titulo,desc)
+
+
+
+    card.appendChild(textos)
+    card.appendChild(image)
+    card.appendChild(variacoes)
+    card.appendChild(carrinho)
+    container.appendChild(card)
 }
 
 
 disciplinas.forEach(criarMenu)
+produtos.forEach(criarCards)
+
